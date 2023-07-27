e48337787bdce69df633c6dad58df62d4a1641dd
Sannu Kumar Ojha <137702131+SannuKumarOjha@users.noreply.github.com>
2023-07-27 20:07:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const MenuBtn = () =>{
+    document.getElementById('OpenNavbar').style.height='100vh' ;
+    document.getElementById('menubtn').style.display ='none';
+}
+const CloseBtn = () =>{
+    document.getElementById('OpenNavbar').style.height='0vh' ;
+    document.getElementById('menubtn').style.display ='block';
+}
